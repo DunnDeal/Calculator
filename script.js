a608b16d1c0b06f0ffe6980a15d902176a42ccdd
DunnDeal <46980019+DunnDeal@users.noreply.github.com>
2019-02-21 16:09:20 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+function operate (
+
+
+
+function add (a,b) {
+	return a + b;
+}
+
+function subtract (a,b) {
+	return a - b;
+}
+
+function sum (array) {
+	return array.reduce((current, total) => total + current, 0)
+}
+
+function multiply (array) {
+	return array.length
+	? array.reduce((accumulater, nextItem) => accumulater * nextItem
+	: 0;
+}
+
+function power(a,b) {
+	return Math.pow(a,b)
+}
+
+function factorial(n) {
+	if (n==0) return 1;
+	let product = 1;
+	for (let i = n; i > 0; i--)
+	product*= i;
+}
+	return product;
+
+module.exports = {
+	add,
+	subtract,
+	sum,
+	multiply,
+    power,
+	factorial
+}
